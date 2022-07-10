375d1763fa43d3936c113fc6272e1950eb1fe033
harry-sketch <harshbisht90@gmail.com>
2022-07-10 15:36:06 +0530

    create: handle file add & deletion with file system

--- index.js
+++ index.js
@@ -10,16 +10,29 @@
 // server.listen(port);
 
 // First Api
+// const http = require("http");
+// const port = 8000;
 
-const http = require("http");
-const port = 8000;
+// const data = require("./data/data");
 
-const data = require("./data/data");
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-type": "applicationjson" });
+//   res.write(JSON.stringify(data));
+//   res.end();
+// });
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "applicationjson" });
-  res.write(JSON.stringify(data));
-  res.end();
-});
+// server.listen(port);
 
-server.listen(port);
+// File System
+const fs = require("fs");
+
+let inputData = process.argv;
+// Write file sync takes two params from which first is for file name (which file to create) & second is the text comes on the fil
+if (inputData[2] === "add") {
+  return fs.writeFileSync(inputData[3], inputData[4]);
+}
+if (inputData[2] === "remove") {
+  return fs.unlinkSync(inputData[3]);
+}
+
+return console.log("unhandled operation");
