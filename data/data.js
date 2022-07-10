ec0dd5c37b2e77124f5edc7e49e13d9e37326fba
harry-sketch <harshbisht90@gmail.com>
2022-07-10 15:19:33 +0530

    create:first Api

---
+++ data/data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    name: "harsh",
+    emailId: "harsh@gmail.com",
+    phone: "111-222-333",
+    country: "India",
+  },
+  {
+    name: "petrick",
+    emailId: "petrick@gmail.com",
+    phone: "111-222-333",
+    country: "U.S.A",
+  },
+];
+
+module.exports = data;
