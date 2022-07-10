9beae256fd3f6f0203eba9b5066180279b0023fb
harry-sketch <harshbisht90@gmail.com>
2022-07-10 15:09:17 +0530

    create: first server

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const { x, y, z } = require("./app");
-console.log(x);
-console.warn(y);
-console.error(z());
+const http = require("http");
+const port = 5000;
 
+const server = http.createServer((req, res) => {
+  res.write("harsh from backend side");
+  res.end();
+});
 
-
-//  Modules are basically of two types
-//  Core & external modules
-// Core modules are also of two types
-// global :-which we dnot have to import(require) like:console filesystem(fs),buffer,http
-// non-global:which we have to import(require) like: npm packages 
+server.listen(port);
